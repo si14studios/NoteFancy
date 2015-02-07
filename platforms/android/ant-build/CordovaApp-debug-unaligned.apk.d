dd78d46f160c67311d572f6590f2652cebb8bb99
joefedden <jfedden@gmail.com>
2015-02-07 15:42:34 -0800

    Build Update
    -

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\Users\Peter\Documents\GitHub\NoteFancy\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Users\Peter\Documents\GitHub\NoteFancy\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\Peter\Documents\GitHub\NoteFancy\platforms\android\ant-build\classes.dex \
-C:\Users\Peter\Documents\GitHub\NoteFancy\platforms\android\CordovaLib\ant-build\classes.jar \
+D:\Program Files (x86)\Github Repositories\NoteFancy\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+D:\Program Files (x86)\Github Repositories\NoteFancy\platforms\android\ant-build\CordovaApp.ap_ \
+D:\Program Files (x86)\Github Repositories\NoteFancy\platforms\android\ant-build\classes.dex \
+D:\Program Files (x86)\Github Repositories\NoteFancy\platforms\android\CordovaLib\ant-build\classes.jar \
